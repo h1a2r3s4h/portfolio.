1722c67f6d021bdd88054e732377c7679b7115fa
h1a2r3s4h <harshitgangwar1601@gmail.com>
2025-10-16 16:53:52 +0530

    cursor change

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,39 @@ import MyWork from "./Components/MyWork/MyWork";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 import BubbleEffect from "./Components/BubbleEffect/BubbleEffect";
+import SnakeCursor from "./Components/SnakeCursor/SnakeCursor"; // 🐍 Custom Snake Cursor
 
 const App = () => {
   return (
     <div>
+      {/* 🐍 Beautiful Snake Trail Cursor */}
+      <SnakeCursor />
+
+      {/* 🌐 Navbar and Visual Effects */}
       <Navbar />
       <BubbleEffect />
 
-      {/* Home Section */}
+      {/* 🏠 Home Section */}
       <section id="home">
         <Hero />
       </section>
 
-      {/* About Section */}
+      {/* 👤 About Section */}
       <section id="about">
         <About />
       </section>
 
-      {/* Portfolio Section */}
+      {/* 💼 Portfolio Section */}
       <section id="portfolio">
         <MyWork />
       </section>
 
-      {/* Contact Section */}
+      {/* 📞 Contact Section */}
       <section id="contact">
         <Contact />
       </section>
 
+      {/* ⚓ Footer */}
       <Footer />
     </div>
   );
